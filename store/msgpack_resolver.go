7ea027a5a590551162bb2294d9dbfae9cef06281
Kelly Sommers <kell.sommers@gmail.com>
2015-05-28 23:59:58 -0400

    Create msgpack_resolver.go

---
+++ store/msgpack_resolver.go
@@ -0,0 +1,8 @@
+package store
+import "reflect"
+
+type MsgPackResolver map[string]reflect.Value
+
+func (self MsgPackResolver) Resolve(name string, arguments []reflect.Value) (reflect.Value, error) {
+	return self[name], nil
+}
