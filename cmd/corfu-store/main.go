dad43d1e6e28c1330c5a65e54dc57356af8a5d1a
Kelly Sommers <kell.sommers@gmail.com>
2015-05-28 23:59:18 -0400

    Create main.go

---
+++ cmd/corfu-store/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/kellabyte/corfu/store"
+)
+
+func main() {
+	log.Info("Creating storage service")
+	storeService := store.New()
+
+	log.Info("Starting storage service")
+	storeService.Listen()
+
+	log.Info("Creating storage client")
+	storeClient := store.NewStoreClient()
+
+	log.Info("Starting storage client")
+	storeClient.Listen()
+}
