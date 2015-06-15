9b40e8afb4b32334caa2b05894fc6347fd17b701
Kelly Sommers <kell.sommers@gmail.com>
2015-06-14 22:36:37 -0400

    Added glide support.

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-export GOPATH=$(pwd)
+mkdir tools
+cd tools
+git clone https://github.com/Masterminds/glide.git
+cd glide
+make bootstrap
+cd ../../
 
-go get github.com/Sirupsen/logrus
-go get github.com/boltdb/bolt
-go get github.com/msgpack-rpc/msgpack-rpc-go/rpc
-go get github.com/paulbellamy/ratecounter
+export GOPATH=$(pwd)/_vendor
+tools/glide/glide install
 
 go build github.com/kellabyte/corfu/cmd/corfu-store
 go build github.com/kellabyte/corfu/cmd/corfu-token
