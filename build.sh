71a297328b948ce32eec79ecdba572d93dc7a37b
Kelly Sommers <kell.sommers@gmail.com>
2015-06-14 21:16:06 -0400

    Added getting dependencies.

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 export GOPATH=$(pwd)
+
+go get github.com/Sirupsen/logrus
+go get github.com/boltdb/bolt
+go get github.com/msgpack-rpc/msgpack-rpc-go/rpc
+go get github.com/paulbellamy/ratecounter
+
 go build github.com/kellabyte/corfu/cmd/corfu-store
 go build github.com/kellabyte/corfu/cmd/corfu-token
