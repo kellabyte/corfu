638ba4dbe47e5c50cebf47350ef41e0c42817283
Kelly Sommers <kell.sommers@gmail.com>
2015-06-14 21:03:36 -0400

    Update store_client.go

--- store/store_client.go
+++ store/store_client.go
@@ -4,30 +4,64 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"net"
 	"github.com/msgpack-rpc/msgpack-rpc-go/rpc"
+	"strconv"
 )
 
 type StoreClient struct {
-
+	rpc *rpc.Session
 }
 
 func NewStoreClient() *StoreClient {
 	return &StoreClient {}
 }
 
-func (storeClient *StoreClient) Listen() error {
+func (client *StoreClient) Listen() error {
 	conn, err := net.Dial("tcp", "127.0.0.1:50000")
 	if err != nil {
 		log.Info("fail to connect to server.")
 		return nil
 	}
-	client := rpc.NewSession(conn, true)
+	client.rpc = rpc.NewSession(conn, true)
 
-	retval, xerr := client.Send("read", 0, 1)
+//	retval, xerr := client.rpc.Send("read", 0, 1)
+//	if xerr != nil {
+//		log.Error(xerr)
+//		return nil
+//	}
+//	log.Info(retval.String())
+
+	return nil
+}
+
+func (client *StoreClient) Append(entry []byte) error {
+	retval, xerr := client.rpc.Send("write", 0, 0, entry)
 	if xerr != nil {
 		log.Error(xerr)
 		return nil
 	}
-	log.Info(retval.String())
+	//log.Info("WRITE: " + retval.Kind().String())
+	log.Info("WRITE: " + strconv.FormatBool(retval.Bool()))
+	return nil
+}
 
+func (client *StoreClient) Read(address uint64) error {
+	retval, xerr := client.rpc.Send("read", 0, 0)
+	if xerr != nil {
+		log.Error(xerr)
+		return nil
+	}
+	log.Info("READ: " + retval.String())
+	return nil
+}
+
+func (client *StoreClient) Fill(address uint64) error {
+	return nil
+}
+
+func (client *StoreClient) Trim(address uint64) error {
+	return nil
+}
+
+func (client *StoreClient) Reconfigure() error {
 	return nil
 }
