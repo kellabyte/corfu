90a6762ce9e89f0b81869fd4a2751c20cbc48fac
Kelly Sommers <kell.sommers@gmail.com>
2015-06-14 21:01:13 -0400

    Delete msgpack_resolver.go

--- store/msgpack_resolver.go
+++
@@ -1,8 +0,0 @@
-package store
-import "reflect"
-
-type MsgPackResolver map[string]reflect.Value
-
-func (self MsgPackResolver) Resolve(name string, arguments []reflect.Value) (reflect.Value, error) {
-	return self[name], nil
-}
